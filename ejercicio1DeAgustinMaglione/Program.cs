e0a95ef5ca44957198310a92eb1048cd7a1904df
agusm <agusm@AGUSTIN>
2022-07-05 23:26:39 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio1DeAgustinMaglione/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1DeAgustinMaglione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pesos, dolar, euro, conversion1,conversion2;
+            Console.WriteLine("Ingrese la cantidad en pesos que quieres convertir:");
+            pesos = double.Parse(Console.ReadLine());
+            dolar = pesos / 230;
+            Console.Write($"La cantidad de dolar que pudes compra es { Math.Truncate( dolar) } dolar ");
+            euro = dolar * 1.17;
+            Console.Write($" y en euros es  { euro } euros ");
+            Console.ReadLine();
+        }
+    
+    }
+  
+}
+
